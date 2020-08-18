c761d0409813b9710ac89a6499628e19060e4903
tngreene <theodorengreene@gmail.com>
2020-08-18 17:10:49 -0400

    Y_QUAD is now (supposedly) working! There were two bugs - reuse of helper function that wasn't general enough and width using the wrong formula. variable names have units now

--- io_scene_xplane_for/forest_tree.py
+++ io_scene_xplane_for/forest_tree.py
@@ -2,7 +2,7 @@ import itertools
 import functools
 import dataclasses
 import math
-import pprint
+from pprint import pprint
 from typing import Tuple, List, Union, Any, Callable, Dict
 import operator
 from operator import attrgetter
@@ -250,7 +250,7 @@ class ForestTree:
                 round(bl.y * size_y),
             )
             self.vert_info.w, self.vert_info.h = (
-                round(tr.x * size_y) - self.vert_info.s,
+                round(tr.x * size_x) - self.vert_info.s,
                 round(tr.y * size_y) - self.vert_info.t,
             )
             print(
@@ -267,17 +267,18 @@ class ForestTree:
 
             def verts_from_edge_global(
                 edge: bpy.types.MeshEdge,
+                matrix_world:mathutils.Matrix
             ) -> Tuple[mathutils.Vector, mathutils.Vector]:
                 return tuple(
                     forest_helpers.round_vec(
-                        self.vert_quad.matrix_world.translation
-                        + self.vert_quad.data.vertices[vi].co
+                        matrix_world.translation
+                        + self.horz_quad.data.vertices[vi].co
                     )
                     for vi in edge.vertices
                 )
 
             left, bottom, right, top = [
-                verts_from_edge_global(edge) for edge in self.vert_quad.data.edges
+                verts_from_edge_global(edge, self.vert_quad.matrix_world) for edge in self.vert_quad.data.edges
             ]
             print(
                 "--- top left bottom right",
@@ -324,7 +325,6 @@ class ForestTree:
                 )
             ]
 
-            )
             bl_uv, br_uv, tl_uv, tr_uv = sorted(uvs[:4])
             self.horz_info.s, self.horz_info.t = (
                 round(bl_uv.x * size_x),
@@ -337,34 +337,44 @@ class ForestTree:
 
             def verts_from_edge_global(
                 edge: bpy.types.MeshEdge,
+                matrix_world:mathutils.Matrix
             ) -> Tuple[mathutils.Vector, mathutils.Vector]:
                 return tuple(
                     forest_helpers.round_vec(
-                        self.horz_quad.matrix_world.translation
+                        matrix_world.translation
                         + self.horz_quad.data.vertices[vi].co
                     )
                     for vi in edge.vertices
                 )
 
-            left, bottom, right, top = [
-                verts_from_edge_global(edge) for edge in self.horz_quad.data.edges
+            horz_left_m, horz_bottom_m, horz_right_m, horz_top_m = [
+                verts_from_edge_global(edge, self.horz_quad.matrix_world) for edge in self.horz_quad.data.edges
             ]
-            origin = self.horz_quad.matrix_world.translation.xy
-            bl_meters = left[1].xy
-            bottom_length = (bottom[0] - bottom[1]).length
-            left_length = (left[0] - left[1]).length
-            uv_scale = mathutils.Vector((self.horz_info.w, self.horz_info.h))
+            # The object origin
+            #TODO: Validate that Y_QUAD origin is TREE trunk
+            object_origin = self.horz_quad.matrix_world.translation.xy
+
+            #bottom_left vertex (meters)
+            horz_bl_m = horz_left_m[1].xy
+
+            horz_bottom_length_m = (horz_bottom_m[0] - horz_bottom_m[1]).length
+            horz_left_length_m = (horz_left_m[0] - horz_left_m[1]).length
             self.horz_info.offset_center_x, self.horz_info.offset_center_y = (
-                round(((origin.x - bl_meters.x) / bottom_length) * uv_scale.x),
-                round(((origin.y - bl_meters.y) / left_length) * uv_scale.y),
+                round(((object_origin.x - horz_bl_m.x) / horz_bottom_length_m) * self.horz_info.w),
+                round(((object_origin.y - horz_bl_m.y) / horz_left_length_m) * self.horz_info.h),
             )
 
-            self.horz_info.quad_width = bottom_length * self.vert_info.h
-            percent_of_way_up_tree_m = (
+            vert_left_m, vert_bottom_m, vert_right_m, vert_top = [
+                verts_from_edge_global(edge, self.vert_quad.matrix_world) for edge in self.vert_quad.data.edges
+            ]
+            vert_bottom_length_m = (vert_bottom_m[0]-vert_bottom_m[1]).length
+
+            self.horz_info.quad_width = (horz_bottom_length_m/vert_bottom_length_m) * self.vert_info.w
+            percent_of_way_up_tree = (
                 self.horz_quad.matrix_world.translation.z / self.vert_info.min_height
             )
             self.horz_info.elevation = round(
-                percent_of_way_up_tree_m * self.vert_info.h
+                percent_of_way_up_tree * self.vert_info.h
             )
             self.horz_info.psi_rotation = round(
                 math.degrees(self.horz_quad.rotation_euler.z)
